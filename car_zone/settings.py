c1f34d490b33ef695ad4575f8975437a88ff406b
Himanshu <hs127010@gmail.com>
2023-07-22 02:24:11 +0530

    Completed Teams Model

--- car_zone/settings.py
+++ car_zone/settings.py
@@ -78,10 +78,17 @@ WSGI_APPLICATION = 'car_zone.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+     'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'car_zone',  
+        'USER': 'root',  
+        'PASSWORD': 'Aman@123',  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',  
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }  
+    }  
 }
 
 
@@ -129,3 +136,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
